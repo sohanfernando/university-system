301e3be95f510f84615454c3fc80e3e966a5da45
sohanfernando <sohanfernando7@gmail.com>
2025-01-20 20:52:08 +0530

    book model update

--- src/main/java/lk/sohan/universitysystem/model/Book.java
+++ src/main/java/lk/sohan/universitysystem/model/Book.java
@@ -1,4 +1,13 @@
 package lk.sohan.universitysystem.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }

bc8b2b0af0e4af3ba17914b50b16ba1d1c186883
sohanfernando <sohanfernando7@gmail.com>
2025-01-20 20:28:54 +0530

    project created

---
+++ src/test/java/lk/sohan/universitysystem/UniversitySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.sohan.universitysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversitySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
